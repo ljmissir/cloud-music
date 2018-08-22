93482f4c34329e353209cb384bef1d9d9589148c
liaojianming <1195108449@qq.com>
2018-08-22 11:12:50 +0800

    commit

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+// https://eslint.org/docs/user-guide/configuring
+
+module.exports = {
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    sourceType: 'module'
+  },
+  env: {
+    browser: true,
+  },
+  // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  rules: {
+    // allow async-await
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    "indent": ["error", 4],
+    'eol-last': 0,
+    'space-before-function-paren': 0,
+    'no-trailing-spaces': ["error", { "skipBlankLines": true }]
+  }
+}
